ce4f34b80a89a44d16441a64619e71c2118c3872
ramble-onward <kyliermp@gmail.com>
2020-06-10 20:50:04 -0700

    create-de-button
    the created button project with assets and a packaged final

---
+++ unity/button/de button/Assets/ClickSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ClickSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public AudioClip sound;
+
+    private Button Button { get { return GetComponent<Button>(); } }
+    private AudioSource Source { get { return GetComponent<AudioSource>(); } }
+
+
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+        Source.clip = sound;
+        Source.playOnAwake = false;
+
+        Button.onClick.AddListener(() => PlaySound());
+    }
+    void PlaySound()
+    {
+        Source.PlayOneShot(sound);
+    }
+}
